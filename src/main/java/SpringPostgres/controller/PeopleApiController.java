b48b2bb547031009a061715b8ddd51d6bd141323
Ivan Silva <ivankuartz@hotmail.com>
2022-11-08 17:11:37 -0300

    Create api controller class

---
+++ src/main/java/SpringPostgres/controller/PeopleApiController.java
@@ -0,0 +1,16 @@
+package SpringPostgres.controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+
+@RequestMapping("/api/people")
+
+public class PeopleApiController {
+
+    // Routes here.
+}
